090219577e294083e02709dc815fbb6f8c73e03b
muzidube <muzidube1@gmail.com>
2021-12-01 16:34:30 +0000

    Fixing default url export

--- client/src/constants/urls.js
+++ client/src/constants/urls.js
@@ -1,3 +1,2 @@
-const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-
-export default IMG_URL;
+// eslint-disable-next-line import/prefer-default-export
+export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
